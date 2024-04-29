0cab4fad859a286fe5bfbcf3265775d8bb2b3a22
2Tucho <albergromero@gmail.com>
2024-04-29 12:43:39 +0200

    first commit

---
+++ src/components/Main/Form/Form.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Formulario = () => {
+  return <form action="">
+  <label htmlFor="">Nombre </label>
+  <input type="text" />
+  <label htmlFor="">Email </label>
+  <input type="email" />
+  <label htmlFor="">Url foto </label>
+  <input type="text" />
+  <label htmlFor="">Edad </label>
+  <input type="number" />
+  <button>Buscar</button>
+</form>
+};
+
+export default Formulario;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from './Form';
+import Card from './Card'
+
+const Main = () => {
+  return <section>
+    <Form/>
+    <Card/>
+  </section>
+};
+
+export default Main;
